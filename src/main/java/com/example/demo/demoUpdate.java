f30b0a0d353983805757822084be0a6a3b50ee81
lxn <hanghailv@126.com>
2019-04-22 09:27:24 +0800

    测试断

--- src/main/java/com/example/demo/demoUpdate.java
+++ src/main/java/com/example/demo/demoUpdate.java
@@ -9,7 +9,7 @@ public class demoUpdate {
         System.out.println("这个是开发的分支");
         System.out.println("这个是master提交的");
         System.out.println("这个开发分支第2个");
-        System.out.println("开发进行rebase");
+        System.out.println("开发进行rebase11");
 
     }
 
